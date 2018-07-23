ff6505e2c4e5ca5504c49a1276afbbb64c6ab0ea
victardius <37775040+victardius@users.noreply.github.com>
2018-07-23 22:40:53 +0200

    Created a first scene with pathfinder

---
+++ Barren Wastes/Assets/Scripts/BlockerTest.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+using Pathfinding;
+
+public class BlockerTest : MonoBehaviour
+{
+    public void Start()
+    {
+        var blocker = GetComponent<SingleNodeBlocker>();
+
+        blocker.BlockAtCurrentPosition();
+    }
+}
